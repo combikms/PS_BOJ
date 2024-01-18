24e3cb2ba30d556f6232d5c5dc257aa4a2f9ec7d
강인석 <combikms@naver.com>
2024-01-18 11:28:36 +0900

    직사각형

---
+++ 직사각형과삼각형/Main.java
@@ -0,0 +1,20 @@
+package Baekjoon.직사각형과삼각형;
+import java.util.Scanner;
+
+public class Main {  
+
+        
+    public static void main(String[] args) {
+             
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.print(A*B);
+        
+        sc.close();        
+
+    }
+    
+}
