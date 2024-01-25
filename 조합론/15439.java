eee9ae23a164f44712ad1df220e95b1fd78eed2b
강인석 <combikms@naver.com>
2024-01-25 10:36:59 +0900

    베라의 패션

---
+++ 조합론/15439.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+
+    public static class balloon {
+        int number;
+        int paper;
+
+        public balloon(int number, int paper) {
+            this.number = number;
+            this.paper = paper;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        bw.write(N * N - N + "");
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+
+}
