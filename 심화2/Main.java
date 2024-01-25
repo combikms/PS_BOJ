8696c73094ccc1688eff191ffb41525321463468
강인석 <combikms@naver.com>
2024-01-25 13:33:18 +0900

    비효율적인 코드

--- 심화2/Main.java
+++ 심화2/Main.java
@@ -2,9 +2,12 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.TreeSet;
 import java.io.IOException;
 
@@ -16,28 +19,29 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
 
-        HashMap<String, Boolean> map = new HashMap<>();
-        map.put("ChongChong", true);
+        ArrayList<Integer> arr = new ArrayList<>();
+        TreeMap<Integer, Integer> map = new TreeMap<>();
 
-        String[] input;
+        int input;
+        int sigma = 0;        
         for (int i = 0; i < N; i++) {
-            input = br.readLine().split(" ");
-            if (map.containsKey(input[0]) && map.get(input[0]) || map.containsKey(input[1]) && map.get(input[1])) {
-                map.put(input[0], true);
-                map.put(input[1], true);
+            input = Integer.parseInt(br.readLine());
+            arr.add(input);
+            sigma += input;
+            if (map.containsKey(input)) {
+                map.put(input, map.get(input) + 1);
             } else {
-                map.put(input[0], false);
-                map.put(input[1], false);
+                map.put(input, 1);
             }
         }
+        arr.sort(Comparator.naturalOrder());
 
-        int count = 0;
-        for (Boolean value : map.values()) {
-            if (value) {
-                count++;
-            }
-        }
-        bw.write(count + "");
+        bw.write(sigma / N + "\n");
+        bw.write(arr.get(N / 2 + 1) + "\n");
+        //여기에 최빈값을 구하는 코드를 작성하려면?
+        bw.write(arr.get(arr.size() - 1) - arr.get(0));
+
+        bw.flush();
 
         bw.close();
         br.close();
