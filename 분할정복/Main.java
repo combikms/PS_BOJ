60c517bf51034f0498e885093e5302c9d451f910
강인석 <combikms@naver.com>
2024-02-01 14:59:04 +0900

    행렬 제곱

--- 분할정복/Main.java
+++ 분할정복/Main.java
@@ -5,17 +5,36 @@ import java.io.OutputStreamWriter;
 import java.io.IOException;
 
 public class Main {
+    private static final long MOD = 1000;
 
-    public static int[][] multiply(int[][] A, int[][] B) {
-        int[][] C = new int[A.length][B[0].length];
+    public static long[][] multiply(long[][] A, long[][] B) {
+        long[][] C = new long[A.length][B[0].length];
         for (int i = 0; i < A.length; i++) {
             for (int j = 0; j < B[0].length; j++) {
                 for (int k = 0; k < A[0].length; k++) {
-                    C[i][j] += A[i][k] * B[k][j];
+                    C[i][j] += (A[i][k] * B[k][j]) % MOD;
                 }
             }
         }
         return C;
+    }
+
+    public static long[][] fastpow(long[][] C, long B) {
+        long[][] result = new long[C.length][C[0].length];
+        if (B == 0) {
+            for (int i = 0; i < C.length; i++) {
+                result[i][i] = 1;
+            }
+            return result;
+        }
+
+        long[][] half = fastpow(C, B / 2);
+
+        if (B % 2 == 0) {
+            return multiply(half, half);
+        } else {
+            return multiply(C, multiply(half, half));
+        }
     }
 
     public static void main(String[] args) throws IOException {
@@ -23,37 +42,25 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String[] input = br.readLine().split(" ");
-        int Na = Integer.parseInt(input[0]);
-        int Ma = Integer.parseInt(input[1]);
+        int N = Integer.parseInt(input[0]);
+        long B = Long.parseLong(input[1]);
 
-        int[][] A = new int[Na][Ma];
-        for (int i = 0; i < Na; i++) {
+        long[][] A = new long[N][N];
+        for (int i = 0; i < N; i++) {
             input = br.readLine().split(" ");
-            for (int j = 0; j < Ma; j++) {
-                A[i][j] = Integer.parseInt(input[j]);
+            for (int j = 0; j < N; j++) {
+                A[i][j] = Long.parseLong(input[j]);
             }
         }
 
-        input = br.readLine().split(" ");
-        int Nb = Integer.parseInt(input[0]);
-        int Mb = Integer.parseInt(input[1]);
-
-        int[][] B = new int[Nb][Mb];
-        for (int i = 0; i < Nb; i++) {
-            input = br.readLine().split(" ");
-            for (int j = 0; j < Mb; j++) {
-                B[i][j] = Integer.parseInt(input[j]);
-            }
-        }
-
-        int[][] C = multiply(A, B);
-
-        for (int i = 0; i < Na; i++) {
-            for (int j = 0; j < Mb; j++) {
-                bw.write(C[i][j] + " ");
+        long[][] C = fastpow(A, B);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                bw.write(C[i][j] % MOD + " ");
             }
             bw.write("\n");
         }
+        bw.flush();
 
         bw.close();
         br.close();
