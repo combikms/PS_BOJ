cf08b50a31ef5439a5afa8c05f44d8e6f062947f
강인석 <combikms@naver.com>
2024-02-07 14:52:12 +0900

    BFS 1

--- 그래프와순회/Main.java
+++ 그래프와순회/Main.java
@@ -5,11 +5,14 @@ import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.io.IOException;
 
 public class Main {
     private static List<List<Integer>> graph = new ArrayList<>();
+    private static Queue<Integer> queue = new LinkedList<>();
     private static boolean[] visited;
     private static int[] order;
     private static int count = 1;
@@ -27,6 +30,21 @@ public class Main {
         for (int x : neighbor) {
             if (!visited[x]) {
                 DFS(V, x);
+            }
+        }
+    }
+
+    public static void BFS(List<List<Integer>> V, int R) {
+        visit(R);
+        queue.offer(R);
+        while (!queue.isEmpty()) {
+            List<Integer> neighbor = new ArrayList<>(V.get(queue.poll()));
+            Collections.sort(neighbor);
+            for (int x : neighbor) {
+                if (visited[x] != true) {
+                    visit(x);
+                    queue.offer(x);
+                }
             }
         }
     }
@@ -55,7 +73,7 @@ public class Main {
             graph.get(v).add(u);
         } // O(M) <= 200,000
 
-        DFS(graph, R);
+        BFS(graph, R);
 
         for (int i = 1; i <= N; i++) {
             bw.write(order[i] + "\n");
