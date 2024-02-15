7b5ab9bcadc12dca776a56bcb6783686f27ff55f
강인석 <combikms@naver.com>
2024-02-15 13:54:19 +0900

    벽 부수고 이동

--- temp/Main.java
+++ temp/Main.java
@@ -8,34 +8,9 @@ import java.io.IOException;
 
 public class Main {
     private static Queue<Integer> queue = new LinkedList<>();
-    private static int[] board = new int[101];
-    private static int[] moves = new int[101];
 
-    public static int BFS() {
-        queue.offer(1);
+    public static void BFS() {
 
-        while (!queue.isEmpty()) {
-            int cur = queue.poll();
-
-            for (int i = 1; i <= 6; i++) {
-                int next = cur + i;
-
-                if (next > 100) {
-                    continue;
-                }
-
-                next = board[next] == 0 ? next : board[next];
-
-                if (next == 1 || moves[next] != 0) {
-                    continue;
-                }
-
-                queue.offer(next);
-                moves[next] = moves[cur] + 1;
-            }
-        }
-
-        return moves[100];
     }
 
     public static void main(String[] args) throws IOException {
@@ -43,15 +18,18 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String[] input = br.readLine().split(" ");
-        int teleports = Integer.parseInt(input[0]) + Integer.parseInt(input[1]);
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
 
-        for (int i = 0; i < teleports; i++) {
-            input = br.readLine().split(" ");
-            board[Integer.parseInt(input[0])] = Integer.parseInt(input[1]);
+        int[][] board = new int[N][M];
+
+        String S;
+        for (int i = 0; i < N; i++) {
+            S = br.readLine();
+            for (int j = 0; j < M; j++) {
+                board[i][j] = S.charAt(j) - '0';
+            }
         }
-
-        bw.write(BFS() + "");
-        bw.flush();
 
         bw.close();
         br.close();
