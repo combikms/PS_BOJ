5bf1b8e60c77f82016b2b19759dbb592549f61c4
kang <combikms@naver.com>
2024-05-30 12:57:59 +0900

    C++ 템플릿

---
+++ temp/template.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+// Data
+
+void Input()
+{
+    // 입력값들
+}
+
+void Solve()
+{
+    // 풀이
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    freopen("Input.txt", "r", stdin); // 제출 시에는 제거
+    Input();
+    Solve();
+
+    return 0;
+}
