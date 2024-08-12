4ccf8187efaebc0a2a84a29b0d4117398c07ec27
combikms <combikms@naver.com>
2024-08-12 20:43:21 +0900

    :tada: 나무 재테크

--- 구현/16235.py
+++ 구현/16235.py
@@ -6,12 +6,12 @@ A = []
 for _ in range(N):
     A.append(list(map(int, sys.stdin.readline().split())))
 
-yb = [[5] * (N) for _ in range(N)]
-tree = []
+yb = [[5] * N for _ in range(N)]
+tree = [[[] for _ in range(N)] for _ in range(N)]
 
 for _ in range(M):
-    temp = list(map(int, sys.stdin.readline().split()))
-    heapq.heappush(tree, [temp[0] - 1, temp[1] - 1, temp[2]])
+    r, c, age = map(int, sys.stdin.readline().split())
+    tree[r - 1][c - 1].append(age)
 
 dr = [1, -1, 0, 0, 1, 1, -1, -1]
 dc = [0, 0, 1, -1, 1, -1, 1, -1]
@@ -19,37 +19,36 @@ dc = [0, 0, 1, -1, 1, -1, 1, -1]
 
 def Sewol():
     global tree
-    live_tree = []
-    dead_tree = []
 
-    # Spring
-    while tree:
-        t = heapq.heappop(tree)
-        if yb[t[0]][t[1]] >= t[2]:
-            yb[t[0]][t[1]] -= t[2]
-            t[2] += 1
-            heapq.heappush(live_tree, t)
-        else:
-            dead_tree.append(t)
-    tree = live_tree
-
-    # Summer
-    while dead_tree:
-        t = dead_tree.pop()
-        yb[t[0]][t[1]] += t[2] // 2
+    # Spring and Summer
+    for r in range(N):
+        for c in range(N):
+            if tree[r][c]:
+                tree[r][c].sort()
+                live_tree = []
+                new_yb = 0
+                for t in tree[r][c]:
+                    if yb[r][c] >= t:
+                        yb[r][c] -= t
+                        live_tree.append(t + 1)
+                    else:
+                        new_yb += t // 2
+                yb[r][c] += new_yb
+                tree[r][c] = live_tree
 
     # Fall
     new_tree = []
-    for t in tree:
-        cr, cc = t[0], t[1]
-        if t[2] % 5 == 0:
-            for i in range(8):
-                nr = cr + dr[i]
-                nc = cc + dc[i]
-                if 0 <= nr < N and 0 <= nc < N:
-                    new_tree.append([nr, nc, 1])
-    while new_tree:
-        heapq.heappush(tree, new_tree.pop())
+    for r in range(N):
+        for c in range(N):
+            for t in tree[r][c]:
+                if t % 5 == 0:
+                    for i in range(8):
+                        nr = r + dr[i]
+                        nc = c + dc[i]
+                        if 0 <= nr < N and 0 <= nc < N:
+                            new_tree.append([nr, nc])
+    for nt in new_tree:
+        tree[nt[0]][nt[1]].append(1)
 
     # Winter
     for r in range(N):
@@ -60,4 +59,9 @@ def Sewol():
 for _ in range(K):
     Sewol()
 
-print(len(tree))
+ans = 0
+for r in range(N):
+    for c in range(N):
+        ans += len(tree[r][c])
+
+print(ans)
